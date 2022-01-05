d4dd19b9ecf6e4a6c39fcc075f625ac6d6b05dcc
SnowmanTheLegend <73532572+SnowmanTheLegend@users.noreply.github.com>
2022-01-05 16:47:44 +0100

    Delete src directory

--- src/JDBC.java
+++
@@ -1,9 +0,0 @@
-import java.sql.SQLException;
-
-public class JDBC {
-	public static void main(String[] args) throws SQLException {
-		// TODO Auto-generated method stub
-			new BlockingInterpreter(new Controller()).handleCmds();
-		
-	}
-}
