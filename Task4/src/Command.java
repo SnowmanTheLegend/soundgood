3cd49e299ca5bcce56f7a4107144b300c3fde22b
SnowmanTheLegend <73532572+SnowmanTheLegend@users.noreply.github.com>
2022-02-03 20:51:33 +0100

    Update Command.java

--- Task4/src/Command.java
+++ Task4/src/Command.java
@@ -1,34 +1,14 @@
 
-	public enum Command {
+		public enum Command {
 		LISTINSTRUMENTS,
 		
-		NEW,
 		
 		RENTINSTRUMENT,
-	    /**
-	     * Creates a new account.
-	     */
+	    
+		
 		TERMINATERENTAL,
-	    /**
-	     * Lists all existing accounts.
-	     */
-	    LIST,
-	    /**
-	     * Deletes the specified account.
-	     */
-	    DELETE,
-	    /**
-	     * Deposits the specified amount to the specified account
-	     */
-	    DEPOSIT,
-	    /**
-	     * Withdraws the specified amount from the specified account
-	     */
-	    WITHDRAW,
-	    /**
-	     * Lists the balance of the specified account.
-	     */
-	    BALANCE,
+	    
+	    
 	    /**
 	     * Lists all commands.
 	     */
